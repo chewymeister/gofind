9d483ded53ab0d5f2dd1a9dcdbfe9dd38843aa7d
Jonathan Qiwen Tsang <jtsang@pivotal.io>
2017-07-28 22:49:22 +0200

    Created more test edge cases
    When substring is provided as query

--- gofind_test.go
+++ gofind_test.go
@@ -3,6 +3,7 @@ package main_test
 import (
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -12,21 +13,67 @@ import (
 
 var _ = Describe("GoFind", func() {
 	var (
-		cmdErr  error
-		cmd     *exec.Cmd
-		session *gexec.Session
+		cmdErr     error
+		cmd        *exec.Cmd
+		session    *gexec.Session
+		currentDir string
+		params     []string
 	)
 
 	BeforeEach(func() {
-		params := []string{"cf-redis-broker"}
-		cmd = exec.Command(binaryPath, params...)
+		var err error
+		currentDir, err = filepath.Abs(filepath.Dir(os.Args[0]))
+		Expect(err).NotTo(HaveOccurred())
+		os.Setenv("GOPATH", filepath.Join(currentDir, "assets"))
 	})
 
-	It("should run the binary", func() {
+	It("returns the correct repo path", func() {
+		params = []string{"foobar"}
+		cmd = exec.Command(binaryPath, params...)
+
 		session, cmdErr = gexec.Start(cmd, os.Stdout, os.Stderr)
 		Expect(cmdErr).NotTo(HaveOccurred())
 		Eventually(session).Should(gexec.Exit(0))
 
-		Eventually(session.Out).Should(gbytes.Say("cf-redis-broker"))
+		expectedRepoPath := filepath.Join(currentDir, "assets", "src", "github.com", "foouser", "foobar")
+		Eventually(session.Out).Should(gbytes.Say(expectedRepoPath))
+	})
+
+	Context("When the query contains a substring", func() {
+		It("returns the correct repo path", func() {
+			params = []string{"foob"}
+			cmd = exec.Command(binaryPath, params...)
+
+			session, cmdErr = gexec.Start(cmd, os.Stdout, os.Stderr)
+			Expect(cmdErr).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+
+			expectedRepoPath := filepath.Join(currentDir, "assets", "src", "github.com", "foouser", "foobar")
+			Eventually(session.Out).Should(gbytes.Say(expectedRepoPath))
+		})
+
+		It("returns the correct repo path", func() {
+			params = []string{"fob"}
+			cmd = exec.Command(binaryPath, params...)
+
+			session, cmdErr = gexec.Start(cmd, os.Stdout, os.Stderr)
+			Expect(cmdErr).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+
+			expectedRepoPath := filepath.Join(currentDir, "assets", "src", "github.com", "foouser", "foobar")
+			Eventually(session.Out).Should(gbytes.Say(expectedRepoPath))
+		})
+
+		It("returns the correct repo path", func() {
+			params = []string{"fbr"}
+			cmd = exec.Command(binaryPath, params...)
+
+			session, cmdErr = gexec.Start(cmd, os.Stdout, os.Stderr)
+			Expect(cmdErr).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+
+			expectedRepoPath := filepath.Join(currentDir, "assets", "src", "github.com", "foouser", "foobar")
+			Eventually(session.Out).Should(gbytes.Say(expectedRepoPath))
+		})
 	})
 })
