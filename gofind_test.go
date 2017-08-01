073ebca66eab587d73e7da109b2eecd8f3bb36e6
Jonathan Qiwen Tsang <jtsang@pivotal.io>
2017-08-01 16:54:37 +0200

    Refactored first example to be simple
    Do not include different users at distribution level

--- gofind_test.go
+++ gofind_test.go
@@ -13,67 +13,72 @@ import (
 
 var _ = Describe("GoFind", func() {
 	var (
-		cmdErr     error
-		cmd        *exec.Cmd
-		session    *gexec.Session
-		currentDir string
-		params     []string
+		gofindCmd *exec.Cmd
+		params    []string
 	)
 
 	BeforeEach(func() {
-		var err error
-		currentDir, err = filepath.Abs(filepath.Dir(os.Args[0]))
-		Expect(err).NotTo(HaveOccurred())
-		os.Setenv("GOPATH", filepath.Join(currentDir, "assets"))
+		testGoPath := filepath.Join(currentDir(), "assets")
+		os.Setenv("GOPATH", testGoPath)
 	})
 
 	It("returns the correct repo path", func() {
 		params = []string{"foobar"}
-		cmd = exec.Command(binaryPath, params...)
+		gofindCmd = exec.Command(binaryPath, params...)
 
-		session, cmdErr = gexec.Start(cmd, os.Stdout, os.Stderr)
-		Expect(cmdErr).NotTo(HaveOccurred())
-		Eventually(session).Should(gexec.Exit(0))
+		session := executeCommand(gofindCmd)
 
-		expectedRepoPath := filepath.Join(currentDir, "assets", "src", "github.com", "foouser", "foobar")
+		expectedRepoPath := repoPath("foobar")
 		Eventually(session.Out).Should(gbytes.Say(expectedRepoPath))
 	})
 
 	Context("When the query contains a substring", func() {
 		It("returns the correct repo path", func() {
-			params = []string{"foob"}
-			cmd = exec.Command(binaryPath, params...)
+			params = []string{"fooba"}
+			gofindCmd = exec.Command(binaryPath, params...)
 
-			session, cmdErr = gexec.Start(cmd, os.Stdout, os.Stderr)
-			Expect(cmdErr).NotTo(HaveOccurred())
-			Eventually(session).Should(gexec.Exit(0))
+			session := executeCommand(gofindCmd)
 
-			expectedRepoPath := filepath.Join(currentDir, "assets", "src", "github.com", "foouser", "foobar")
+			expectedRepoPath := repoPath("foobar")
 			Eventually(session.Out).Should(gbytes.Say(expectedRepoPath))
 		})
 
 		It("returns the correct repo path", func() {
-			params = []string{"fob"}
-			cmd = exec.Command(binaryPath, params...)
+			params = []string{"foob"}
+			gofindCmd = exec.Command(binaryPath, params...)
 
-			session, cmdErr = gexec.Start(cmd, os.Stdout, os.Stderr)
-			Expect(cmdErr).NotTo(HaveOccurred())
-			Eventually(session).Should(gexec.Exit(0))
+			session := executeCommand(gofindCmd)
 
-			expectedRepoPath := filepath.Join(currentDir, "assets", "src", "github.com", "foouser", "foobar")
+			expectedRepoPath := repoPath("foobar")
 			Eventually(session.Out).Should(gbytes.Say(expectedRepoPath))
 		})
 
 		It("returns the correct repo path", func() {
 			params = []string{"fbr"}
-			cmd = exec.Command(binaryPath, params...)
+			gofindCmd = exec.Command(binaryPath, params...)
 
-			session, cmdErr = gexec.Start(cmd, os.Stdout, os.Stderr)
-			Expect(cmdErr).NotTo(HaveOccurred())
-			Eventually(session).Should(gexec.Exit(0))
+			session := executeCommand(gofindCmd)
 
-			expectedRepoPath := filepath.Join(currentDir, "assets", "src", "github.com", "foouser", "foobar")
+			expectedRepoPath := repoPath("foobar")
 			Eventually(session.Out).Should(gbytes.Say(expectedRepoPath))
 		})
 	})
 })
+
+func repoPath(repoName string) string {
+	return filepath.Join(currentDir(), "assets", "src", "simple", repoName)
+}
+
+func currentDir() string {
+	currentdirPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	Expect(err).NotTo(HaveOccurred())
+	return currentdirPath
+}
+
+func executeCommand(cmd *exec.Cmd) *gexec.Session {
+	session, cmdErr := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+	Expect(cmdErr).NotTo(HaveOccurred())
+	Eventually(session, "5s").Should(gexec.Exit(0))
+
+	return session
+}
